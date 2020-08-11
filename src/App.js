96f31ae5a00dadc07b91ef256c830127269cefa4
tester <aiforeveryoneai@gmail.com>
2020-08-11 14:40:53 +0500

    Update App.js
    Added basic Redux functionality and logged it on console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { createStore } from "redux";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        state = state + action.payload;
+        break;
+      case "SUBTRACT":
+        state = state - action.payload;
+        break;
+    }
+    return state;
+  };
+  const store = createStore(reducer, 1);
+
+  store.subscribe(() => {
+    console.log("Store updated", store.getState());
+  });
+  store.dispatch({
+    type: "ADD",
+    payload: 10,
+  });
+  store.dispatch({
+    type: "SUBTRACT",
+    payload: 2,
+  });
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
